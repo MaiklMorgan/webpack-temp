3d970667604ddb18392c53fc2a878398cd8cd07a
Миша Мирончук <skorpion.9337@gmai.com>
2018-11-03 20:11:30 +0000

    add loader

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,13 @@
 
-console.log(' - ready steady go! ')
-//  
+console.log(' - ready steady go! ', name, isDev, isProd)
+//  халоу
+
+
+// Напишемо щось у стилі es6
+// чому?
+// Бо круто і треба писати код нормально)
+// Наприклад розбивати на модулі і юзати їх, як нижче
+import devs from './users'
+
+console.log('DEVS:', devs)
+
--- config/webpack.config.babel.js
+++ config/webpack.config.babel.js
@@ -50,6 +50,17 @@ export default ({mode='production'}) => {
     // `${__dirname це для  тут тут
     // Це для того, щоб точно знайти місце файлу. Дивись  гуд
     
+    let extractCSS = new ExtractTextPlugin({
+        disable: false,
+        allChunks: true,
+        filename: 'static/style/css-[name].css?v=[chunkhash]',
+    })
+    
+    let extractLESS = new ExtractTextPlugin({
+        filename: '[name].css?v=[chunkhash]',
+        disable: false,
+    })
+    
     return {
         mode,
         entry: {
@@ -71,6 +82,7 @@ export default ({mode='production'}) => {
                 ? '[name].js?v=[hash]'
                 : '[name]-[hash].js',
             path: `${__dirname}/../client/dist`,
+            chunkFilename: '[name].bundle.js',
         },
         /*
             - Це понятно що і навіщо?
@@ -84,13 +96,76 @@ export default ({mode='production'}) => {
             - index ж є в статік
         */
         devtool: isDev? 'cheap-inline-module-source-map': false,
+        module: {
+            rules: [{
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { 'modules': false }],
+                            // ['es2015', { 'modules': false }],
+                            // 'stage-0',
+                        ]
+                    }
+                }],
+                exclude: /node_modules/
+            }, {
+                // CSS
+                test: /\.css$/,
+                use: extractCSS.extract([ 'css-loader?minimize',/* 'css-loader?minimize'*//*, 'postcss-loader'*/ ]),
+            }, {
+                // LESS
+                test: /\.less$/,
+                use: extractLESS.extract({
+                    use: [/*{
+                        loader: 'vue-style-loader',
+                    },*/ {
+                        loader: 'css-loader',
+                    }, {
+                        loader: 'less-loader', 
+                        options: {
+                            paths: [
+                                path.resolve(__dirname, '..', 'client', 'src', 'less'),
+                                path.resolve(__dirname, '..', 'node_modules'),
+                            ],
+                        },
+                    }],
+                    fallback: 'style-loader'
+                })
+            }, {
+                // Тут треба сконфіжити паг, лесс і навіть js
+                test: /\.pug$/,
+                // use: [{
+                //     loader: 'pug-plain-loader',
+                // }],
+                oneOf: [
+                    // this applies to <template lang="pug"> in Vue components
+                    {
+                        resourceQuery: /^\?vue/,
+                        use: ['pug-plain-loader']
+                    },
+                    // this applies to pug imports inside JavaScript
+                    {
+                        // Постав ще цей лоадер 'raw-loader'
+                        use: ['raw-loader', 'pug-plain-loader']
+                    }
+                ],
+            }],
+        },
         plugins: [
             
             
             // Знаєш, що це?
+            //  про що? те що вище?
+            //  режими дев прод
+            // Що з ними робиться? І для чого? 
+            //  
             new webpack.DefinePlugin({
                 isDev,
                 isProd,
+                name: JSON.stringify(`Misha::${__dirname}`),// Хз, що це я зробив? шукаєш де я
+                // Го в індекс.js
                 'process.env.NODE_ENV': JSON.stringify('production')
             }),
             
@@ -100,6 +175,16 @@ export default ({mode='production'}) => {
                 to: `${__dirname}/../client/dist`,
                 from: `${__dirname}/../client/static`,
             }]),
+            
+            
+            new HTMLWebpackPlugin({
+                path: `${__dirname}/../client/dist`,
+                // filename: 'index.html',
+                template: 'client/src/index.pug',
+            }),
+            
+            extractCSS,
+            extractLESS,
             
         ],
     }
